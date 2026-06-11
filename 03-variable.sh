b1e46a6c56e416d822a3c25a3721ecbcec90cf20
Siva Bhimisetty <bhimisetty.devops@gmail.com>
2026-06-11 17:59:43 +0530

    variables

---
+++ 03-variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PERSON1=MODI
+PERSON2=TRUMP
+
+echo "$PERSON1: Hey $PERSON2, how are you."
+echo "$PERSON2: I'm doing good, how are you doing?"
+echo "$PERSON1: I'm doing good, but I have a question for you."
+echo "$PERSON2: Sure, ask me anything."
+echo "$PERSON1: Do you think we can colonize Mars?"
+echo "$PERSON2: Yes, I believe we can colonize Mars in the future."
+echo "$PERSON1: That's great to hear. I think we should start working on it as soon as possible."
+echo "$PERSON2: I agree, we should start working on it as soon as possible."
+echo "$PERSON1: Let's do it!"
+echo "$PERSON2: Let's do it!"
