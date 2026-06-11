48546517597219783e67c01c9520c42de8f62a3e
Siva Bhimisetty <bhimisetty.devops@gmail.com>
2026-06-11 17:55:19 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Trump: Hey musk, how are you."
+echo "Musk: I'm doing good, how are you doing?"
+echo "Trump: I'm doing good, but I have a question for you."
+echo "Musk: Sure, ask me anything."
+echo "Trump: Do you think we can colonize Mars?"
+echo "Musk: Yes, I believe we can colonize Mars in the future."
+echo "Trump: That's great to hear. I think we should start working on it as soon
+as possible."
+echo "Musk: I agree, we should start working on it as soon as possible."
+echo "Trump: Let's do it!"
+echo "Musk: Let's do it!"
