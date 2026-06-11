48a7849a578a3a3fc0bad1fee7e360cc6ffe9024
Siva Bhimisetty <bhimisetty.devops@gmail.com>
2026-06-11 21:07:41 +0530

    initiated

---
+++ 10-installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash     #shebang line, it tells the system that this script should be run using the bash shell
+
+USERID=$(id -u) # this command gets the user ID of the current user and stores it in
+
+if [ $USERID -eq 0 ]
+then
+    echo "you are root user, you can do anything..."
+else
+    echo "you are not root user, you cannot install the software..."
+
+fi
+
+dnf install mysql -y
