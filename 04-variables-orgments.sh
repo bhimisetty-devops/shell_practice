29a47eca0373360dce736ca057c3d65353e79a66
Siva Bhimisetty <bhimisetty.devops@gmail.com>
2026-06-11 18:04:40 +0530

    variables

---
+++ 04-variables-orgments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hey $PERSON2, how are you."
+echo "$PERSON2: I'm doing good, how are you doing?"
+echo "$PERSON1: I'm doing good, but I have a question for you."
+echo "$PERSON2: Sure, ask me anything."
+echo "$PERSON1: Do you think we can colonize Mars?"
