27c8be7240841cbabf1924b1629c474c9b8d8a0a
Siva Bhimisetty <bhimisetty.devops@gmail.com>
2026-06-11 21:45:38 +0530

    initiated

---
+++ 11-mysql-installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(id -u )
+
+if [ $USERID -eq 0 ]
+then
+    echo "You are root user, you can install the packages"
+else
+    echo "You are not root user, you cannot install the packages"
+fi
+
+yum list available | grep mysql
+if [ $? -eq 0 ]
+then
+    echo "MySQL package is available and ready to install"
+    yum install mysql -y
+else
+    echo "MySQL package is not available in the yum repository, please check your repository settings"
+fi
+
