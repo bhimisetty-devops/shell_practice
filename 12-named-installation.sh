129270a9d534dada7c092d6e4894ace026fcd927
Siva Bhimisetty <bhimisetty.devops@gmail.com>
2026-06-12 10:10:02 +0530

    initiated

--- 12-named-installation.sh
+++ 12-named-installation.sh
@@ -4,7 +4,6 @@ USERID=$( id -u )
 if [ $USERID -eq 0 ]
 then
     echo "you are a root user, so you have a prmission to install the package"
-
 else
     echo "you are not a root user, so please run with root access"
     exit 1
@@ -14,6 +13,10 @@ yum list installed | grep bind
 if [ $? -eq 0 ]
 then
     echo "bind package is already installed, removing it first.."
+else
+    echo "bind package is not installed, proceeding with installation..."
+    exit 1
+fi
 yum remove bind -y
 if [ $? -eq 0 ]
 then
